847c10991206ae0f33345bf27ba91b69fa8798c2
Brandon Pearson <blp35@students.uwf.edu>
2021-11-10 14:23:58 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
